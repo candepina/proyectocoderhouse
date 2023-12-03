a8c7c5959f7a85ec79c1309d442832a7f40b08a5
candepina <candepina475@gmail.com>
2023-12-03 11:37:35 -0300

    corrección de errores: diseño responsive, ya no hay dos navs en el header

--- scripts/menuhamburguesa.js
+++ scripts/menuhamburguesa.js
@@ -1,22 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
     const icon = document.querySelector("#burguerIcon");
-    const menu = document.querySelector("#burguerMenu");
-   
+    const menu = document.querySelector("#navsanjovr");
+    const cerrarMenuBtn = document.getElementById("cerrarMenu");
 
     icon.addEventListener("click", () => {
         menu.classList.toggle("mostrar");
     });
-});
-const cerrarMenuBtn = document.getElementById("cerrarMenu");
 
-cerrarMenuBtn.addEventListener('click', () => {
-    burguerMenu.classList.remove('mostrar');
-});
+    cerrarMenuBtn.addEventListener('click', () => {
+        menu.classList.remove('mostrar');
+    });
 
-
-const enlacesMenu = burguerMenu.querySelectorAll('ul.navLinks li a');
-enlacesMenu.forEach(enlace => {
-    enlace.addEventListener('click', () => {
-        burguerMenu.classList.remove('mostrar');
+    const enlacesMenu = menu.querySelectorAll('ul.navLinks li a');
+    enlacesMenu.forEach(enlace => {
+        enlace.addEventListener('click', () => {
+            menu.classList.remove('mostrar');
+        });
     });
 });
